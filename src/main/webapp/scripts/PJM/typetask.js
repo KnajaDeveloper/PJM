3f400a4c250571dc5667082858b729c479ac96a2
Tanagorn_s <its-tanagorn_s@softsquaregroup.com>
2016-02-15 12:36:35 +0700

    Dev CheckData

--- src/main/webapp/scripts/PJM/typetask.js
+++ src/main/webapp/scripts/PJM/typetask.js
@@ -2,114 +2,7 @@ $("#checkAll").click(function () {
     $(".check").prop('checked', $(this).prop('checked'));
 });
 
-
-$('[id^=btnM]').click(function () {
-    var id = this.id.split('M')[1];
-    if (id == 'Cancel') {
-
-        $('#typeTaskCode').popover('hide');
-        $('#typeTaskName').popover('hide');
-        $('#typeTaskCode').val(null);
-        $('#typeTaskName').val(null);
-
-
-    } else {
-
-        if ($("#typeTaskCode").val() == "") {
-
-            $("#typeTaskCode").popover('show');
-
-        } else if ($("#typeTaskName").val() == "") {
-
-            $("#typeTaskName").popover('show');
-
-        } else {
-
-
-            var typeTask = {
-                typeTaskCode: $("#typeTaskCode").val(),
-                typeTaskName: $("#typeTaskName").val()
-            };
-
-
-            $.ajax({
-                type: "POST",
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                headers: {
-                    Accept: "application/json"
-                },
-                url: contextPath + '/typetasks',
-                data: JSON.stringify(typeTask),
-                complete: function (xhr) {
-                    if (xhr.status == 201) {
-                        if (id == 'Add') {
-                            $('#add').modal('hide');
-                            bootbox.alert("บันทึกข้อมูลสำเร็จ");
-                        }
-
-                        $('#typeTaskCode').val(null);
-                        $('#typeTaskName').val(null);
-                    } else if (xhr.status == 500) {
-
-                    }
-                },
-                async: false /*ต้องใส่*/
-            });
-
-        }
-    }
-
-});
-
-
-/*$('#search').click(function () {
-
-    var responseHeader = $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        headers: {
-            Accept: "application/json"
-        },
-        url: contextPath + '/typetasks/findAllProject',
-        data: {
-            findTypeCode: $('#sTypeTaskCode').val(),
-            findTypeName: $('#sTypeTaskName').val()
-        },
-        complete: function (xhr) {
-
-        },
-        async: false /!*ต้องใส่*!/
-    });
-
-
-    var Obj = jQuery.parseJSON(responseHeader.responseText);
-    var tableData;
-
-    $('#tbody').empty();
-
-    $.each(Obj, function(kay, val) {
-        tableData = '<tr>'
-            + '<td class="text-center">'
-            + '<input class="check" type="checkbox" id="checkDelete"/>'
-            + '</td>'
-            + '<td class="text-center">'
-            + '<button id="btnEdit" type="button" class="btn btn-info" data-toggle="modal" data-target="#edit" data-backdrop="static"><span  class="glyphicon glyphicon-pencil" aria-hidden="true" ></span></button>'
-            + '</td>'
-            + '<td id="tdTypeTaskCode">'
-            + val["typeTaskCode"]
-            + '</td>'
-            + '<td id="tdTypeTaskName">'
-            + val["typeTaskName"]
-            + '</td>'
-            + '</tr>';
-
-        $('#tbody').append(
-            tableData
-        );
-    });
-});*/
+//------------------------------------------------------------------------------------
 
 $('#search').click(function () {
     searchData();
@@ -118,37 +11,38 @@ $('#search').click(function () {
 
 var paggination = Object.create(UtilPaggination);
 
-
-paggination.setEventPaggingBtn("paggingSimple",paggination);
-paggination.loadTable = function loadTable (jsonData) {
-    if(jsonData.length <= 0){
+paggination.setEventPaggingBtn("paggingSimple", paggination);
+paggination.loadTable = function loadTable(jsonData) {
+    if (jsonData.length <= 0) {
         MessageUtil.alertBootBoxMessage({
-            title : Message.NotHaveData,
+            title: Message.NotHaveData,
             buttons: {
                 cancel: {
-                    label:  Button.Approve,
+                    label: Button.Approve,
                 }
             }
         });
     }
 
     $('#tbody').empty();
-    var link = "";
+
+    var i = 1;
     var tableData = "";
-    jsonData.forEach(function(value){
+    jsonData.forEach(function (value) {
+
         tableData = ''
-            +'<tr>'
+            + '<tr>'
             + '<td class="text-center">'
-            + '<input class="check" type="checkbox" id="checkDelete"/>'
+            + '<input class="check" type="checkbox" id="checkDelete' + i + '"/>'
             + '</td>'
             + '<td class="text-center">'
-            + '<button id="btnEdit" type="button" class="btn btn-info" data-toggle="modal" data-target="#edit" data-backdrop="static"><span  class="glyphicon glyphicon-pencil" aria-hidden="true" ></span></button>'
+            + '<button id="btnEdit' + i + '" type="button" class="btn btn-info" data-toggle="modal" data-target="#edit" data-backdrop="static"><span  class="glyphicon glyphicon-pencil" aria-hidden="true" ></span></button>'
             + '</td>'
-            + '<td id="tdTypeTaskCode">'
-            + val["typeTaskCode"]
+            + '<td id="tdTypeTaskCode' + i + '">'
+            + value.code
             + '</td>'
-            + '<td id="tdTypeTaskName">'
-            + val["typeTaskName"]
+            + '<td id="tdTypeTaskName' + i++ + '">'
+            + value.name
             + '</td>'
             + '</tr>';
 
@@ -156,24 +50,128 @@ paggination.loadTable = function loadTable (jsonData) {
     });
 };
 
-function searchData(){
+function searchData() {
     var dataJsonData = {
         findTypeCode: $('#sTypeTaskCode').val(),
         findTypeName: $('#sTypeTaskName').val()
-    }
+    };
 
     paggination.setOptionJsonData({
-        url:contextPath+"/typetasks/testPaggingData",
-        data:dataJsonData
+        url: contextPath + "/typetasks/testPaggingData",
+        data: dataJsonData
     });
 
     paggination.setOptionJsonSize({
-        url:contextPath+"/typetasks/testPaggingSize",
-        data:dataJsonData
+        url: contextPath + "/typetasks/testPaggingSize",
+        data: dataJsonData
     });
 
     paggination.search(paggination);
 }
+
+//------------------------------------------------------------------------------------
+
+$("#table").on("click", '[id^=btnEdit]', function () {
+    var id = this.id.split('t')[2];
+    $("#eTypeTaskCode").val($("#tdTypeTaskCode" + id).text());
+    $("#eTypeTaskName").val($("#tdTypeTaskName" + id).text());
+});
+//------------------------------------------------------------------------------------
+var sizedata ;
+//ส่ง ค่าเข้าไป check ข้างใน active แล้ว return กลับมาเป็นเลข แล้วเอามา check
+function checkData() {
+    var checkTypeTask = {
+        //ตรงกับ RequestParam value
+        checkTypeCode: $("#aTypeTaskCode").val()
+    };
+
+    var checkdata = $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            Accept: "application/json"
+        },
+        url: contextPath + '/typetasks/checkAllProject',
+        data: checkTypeTask,
+        complete: function (xhr) {
+
+        },
+        async: false /*ต้องใส่*/
+    });
+
+    sizedata = jQuery.parseJSON(checkdata.responseText);
+}
+//------------------------------------------------------------------------------------
+
+$('[id^=btnM]').click(function () {
+    var id = this.id.split('M')[1];
+    if (id == 'Cancel') {
+
+        $('#aTypeTaskCode').popover('hide');
+        $('#aTypeTaskName').popover('hide');
+        $('#aTypeTaskCode').val(null);
+        $('#aTypeTaskName').val(null);
+
+
+    } else {
+
+        if ($("#aTypeTaskCode").val() == "") {
+
+            $("#aTypeTaskCode").popover('show');
+
+        } else if ($("#aTypeTaskName").val() == "") {
+
+            $("#aTypeTaskName").popover('show');
+
+        } else {
+
+            var typeTask = {
+                typeTaskCode: $("#aTypeTaskCode").val(),
+                typeTaskName: $("#aTypeTaskName").val()
+            }
+
+
+            checkData();
+
+            if (sizedata == 0) {
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    headers: {
+                        Accept: "application/json"
+                    },
+                    url: contextPath + '/typetasks',
+                    data: JSON.stringify(typeTask),
+                    complete: function (xhr) {
+
+                        if (xhr.status == 201) {
+                            if (id == 'Add') {
+
+                                $('#add').modal('hide');
+                                bootbox.alert("บันทึกข้อมูลสำเร็จ");
+                            }
+
+                            $('#aTypeTaskCode').val(null);
+                            $('#aTypeTaskName').val(null);
+                        } else if (xhr.status == 500) {
+
+                        }
+                    },
+                    async: false /*ต้องใส่*/
+                });
+            } else {
+                bootbox.alert("มีข้อมูลแล้ว");
+            }
+        }
+    }
+});
+
+
+
+
+
 
 
 
